6d365eed85446e1cf183901563bbdf109d2de474
Neal Peteros <n.peteros2003@gmail.com>
2026-09-25 20:05:54 +0800

    feat: added topic and author pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,5 +17,36 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     lastModified: article.publishedAt ?? article.updatedAt,
   }));
 
-  return [{ url: SITE_URL }, ...sectionEntries, ...articleEntries];
+  const topicLastModified = new Map<string, Date>();
+  const authorLastModified = new Map<string, Date>();
+  for (const article of articles) {
+    const modified = article.publishedAt ?? article.updatedAt;
+    for (const tagSlug of article.tagSlugs) {
+      keepLatest(topicLastModified, tagSlug.toLocaleLowerCase(), modified);
+    }
+    keepLatest(authorLastModified, article.authorId, modified);
+  }
+
+  const topicEntries: MetadataRoute.Sitemap = Array.from(topicLastModified, ([tagSlug, lastModified]) => ({
+    url: `${SITE_URL}/topic/${tagSlug}`,
+    lastModified,
+  }));
+
+  const authorEntries: MetadataRoute.Sitemap = Array.from(authorLastModified, ([authorId, lastModified]) => ({
+    url: `${SITE_URL}/author/${authorId}`,
+    lastModified,
+  }));
+
+  return [
+    { url: SITE_URL },
+    ...sectionEntries,
+    ...topicEntries,
+    ...authorEntries,
+    ...articleEntries,
+  ];
+}
+
+function keepLatest(map: Map<string, Date>, key: string, date: Date) {
+  const current = map.get(key);
+  if (!current || date > current) map.set(key, date);
 }
